b99fda3820bbc6d0f3221a83317a83011ab3ad93
DikaDev-All <muhammaddika43211@gmail.com>
2025-04-26 18:36:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function goToWhatsApp() {
     const game = document.getElementById("game").value;
     const jumlah = document.getElementById("jumlah").value;
     const msg = `Halo, saya ingin top-up ${game}\nID: ${user}\nJumlah: ${jumlah}`;
-    const url = "https://wa.me/6282165354788?text=" + encodeURIComponent(msg);
+    const url = "https://wa.me/6285189450177?text=" + encodeURIComponent(msg);
     window.open(url, '_blank');
     return false;
 }
